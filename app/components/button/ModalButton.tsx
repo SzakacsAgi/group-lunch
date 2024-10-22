af0b3b4bf2f2d91154ac9f6a9ca9d3103f093c65
Ágnes Szakács <szakacs.agi21@gmail.com>
2024-10-22 14:12:12 +0200

    Fix some bugs related to create and edit restaurants (#9)
    Redefined add and edit restaurants structure, fix some bugs

--- app/components/button/ModalButton.tsx
+++ app/components/button/ModalButton.tsx
@@ -3,6 +3,9 @@
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from '@nextui-org/react'
 import React, { FunctionComponent, ReactElement } from 'react'
 import { SupportedModalButtonTypes } from '../../../interface'
+import { useFileOperations } from '../../api/useFileOperations'
+import { useRecoilState } from 'recoil'
+import { uploadedImageTrash } from '../../../utils/atoms'
 
 interface ModalButtonProps {
   buttonPurpose: SupportedModalButtonTypes
@@ -13,42 +16,49 @@ interface ModalButtonProps {
   submitText?: string
   cancelText?: string
   onSubmit?: () => void
-  handleOnClose?: (() => void) | (() => Promise<void>)
 }
 
 const ModalButton: FunctionComponent<ModalButtonProps> = ({
-  buttonPurpose,
   buttonText,
   modalHeaderText,
   withButtons,
   submitText,
   cancelText,
   modalContent,
+  buttonPurpose,
   onSubmit,
   handleOnClose,
 }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const { deleteFile } = useFileOperations()
+  const [uploadedImageId, setUploadedImageId] = useRecoilState(uploadedImageTrash)
+
   const detectContent = () => {
     switch (buttonPurpose) {
       case SupportedModalButtonTypes.EDIT_RESTAURANT:
       case SupportedModalButtonTypes.CREATE_RESTAURANT:
-        return React.cloneElement(modalContent, { onClose: handleCancel })
+        return React.cloneElement(modalContent, { onClose: onClose })
+      case SupportedModalButtonTypes.DELETE_RESTAURANT:
+        return modalContent
       default:
         return modalContent
     }
   }
-  const handleSubmit = () => {
-    if (onSubmit) {
-      onSubmit()
-      onClose()
-    } else {
-      console.warn('onSubmit is not defined')
+
+  const deleteUploadedFileOnClose = async () => {
+    try {
+      if (uploadedImageId) {
+        await deleteFile(uploadedImageId)
+      }
+      setUploadedImageId(null)
+    } catch (error) {
+      console.error(error)
     }
   }
 
-  const handleCancel = async () => {
-    handleOnClose && (await handleOnClose())
+  const handleClose = async () => {
     onClose()
+    await deleteUploadedFileOnClose()
   }
 
   return (
@@ -56,23 +66,19 @@ const ModalButton: FunctionComponent<ModalButtonProps> = ({
       <Button onPress={onOpen} color='primary'>
         {buttonText}
       </Button>
-      <Modal backdrop='blur' isOpen={isOpen} onOpenChange={handleCancel} placement='top-center'>
+      <Modal backdrop='blur' isOpen={isOpen} onClose={handleClose} hideCloseButton onOpenChange={() => onClose()}>
         <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader>{modalHeaderText}</ModalHeader>
-              <ModalBody>{detectContent()}</ModalBody>
-              {withButtons && (
-                <ModalFooter>
-                  <Button color='primary' variant='flat' onClick={handleSubmit}>
-                    {submitText}
-                  </Button>
-                  <Button color='primary' onPress={onClose}>
-                    {cancelText}
-                  </Button>
-                </ModalFooter>
-              )}
-            </>
+          <ModalHeader>{modalHeaderText}</ModalHeader>
+          <ModalBody>{detectContent()}</ModalBody>
+          {withButtons && (
+            <ModalFooter>
+              <Button color='primary' variant='flat' onClick={onSubmit}>
+                {submitText}
+              </Button>
+              <Button color='primary' onPress={onClose}>
+                {cancelText}
+              </Button>
+            </ModalFooter>
           )}
         </ModalContent>
       </Modal>
